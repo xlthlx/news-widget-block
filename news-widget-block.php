cfbc16c70acc6d2275baeae3c9bde8a5daed5640
xlthlx <xlthlx@gmail.com>
2024-11-10 12:09:05 +0000

    Add main plugin file

---
+++ news-widget-block.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     News Widget Block
+ * Plugin URI:      https://github.com/xlthlx/news-widget-block
+ * Description:     News Widget Block is a WordPress plugin that adds a custom block with a setting to choose the number of articles to display.
+ * Author:          xlthlx <xlthlx@gmail.com>
+ * Author URI:      https://piccioni.london
+ * Text Domain:     news-widget-block
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         News_Widget_Block
+ */
+
