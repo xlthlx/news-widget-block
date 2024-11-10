680beef02afd237c879c384d05fc87d823425cbb
xlthlx <xlthlx@gmail.com>
2024-11-10 15:29:51 +0000

    Move the block into a subfolder to manage multiple blocks

---
+++ blocks/build/us-news/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1fe783a68482c0adff33');
--- blocks/class-blocks-config.php
+++ blocks/class-blocks-config.php
@@ -1,16 +1,8 @@
 <?php
 /**
- * Plugin Name:       Us News
- * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.6
- * Requires PHP:      7.2
- * Version:           0.1.0
- * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       us-news
+ * Manage blocks.
  *
- * @package CreateBlock
+ * @package News_Widget_Block
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -25,6 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_us_news_block_init() {
-	register_block_type( __DIR__ . '/build' );
+	register_block_type( __DIR__ . '/build/us-news/' );
 }
 add_action( 'init', 'create_block_us_news_block_init' );
